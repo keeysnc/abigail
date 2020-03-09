c763859e7ebe931cfae5e899b9f3440bbb64a761
Nathan Keeyss <keeysnc@gmail.com>
2020-03-08 23:35:08 -0400

    add intl and i18n setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,35 @@
 import React from "react"
-import { Link } from "gatsby"
-
+import { FormattedMessage, Link, useIntl } from "gatsby-plugin-intl"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  const intl = useIntl()
+  return (
+    <Layout>
+      <SEO
+        lang={intl.locale}
+        title={intl.formatMessage({ id: "title" })}
+        keywords={[`gatsby`, `application`, `react`]}
+      />
+      <h1>
+        <FormattedMessage id="hello" />
+      </h1>
+      <p>
+        <FormattedMessage id="welcome" />
+      </p>
+      <p>
+        <FormattedMessage id="build" />
+      </p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">
+        <FormattedMessage id="go_page2" />
+      </Link>
+    </Layout>
+  )
+}
 
 export default IndexPage
