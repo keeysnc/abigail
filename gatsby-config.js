10308e3f5a24b5c4a0664295774d8312c380b824
Nathan Keeyss <keeysnc@gmail.com>
2020-03-23 02:25:05 -0400

    remove plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,21 +17,6 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-plugin-netlify-cms`,
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `markdown-pages`,
-        path: `${__dirname}/src/markdown-pages`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/src/markdown-pages`,
-        name: `markdown-pages`,
-      },
-    },
-    `gatsby-transformer-remark`,
-    {
       resolve: `gatsby-plugin-netlify-identity`,
       options: {
         url: `https://your-identity-instance-here.netlify.com/`, // required!
