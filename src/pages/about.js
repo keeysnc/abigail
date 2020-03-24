258e1425284806f096dde69bd095c8878c26956b
Nathan Keeyss <keeysnc@gmail.com>
2020-03-24 03:43:02 -0400

    add absolute file path to about page for content query

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { FormattedMessage, injectIntl, navigate } from "gatsby-plugin-intl"
+import { injectIntl } from "gatsby-plugin-intl"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout"
@@ -7,7 +7,7 @@ import SEO from "../components/seo"
 import Paragraph from "../components/paragraphSection"
 
 const About = ({ intl }) => {
-  const data = useStaticQuery(graphql`
+  const dataAbout = useStaticQuery(graphql`
     query {
       about: markdownRemark(
         id: { eq: "f6fcb3c5-3786-550a-8ead-653706d9165f" }
@@ -16,6 +16,7 @@ const About = ({ intl }) => {
           description_en
           description_es
         }
+        fileAbsolutePath
       }
     }
   `)
@@ -26,8 +27,8 @@ const About = ({ intl }) => {
         title={intl.formatMessage({ id: "title_page2" })}
       />
       <Paragraph
-        // message_es={data.about.frontmatter.description_es}
-        // message_en={data.about.frontmatter.description_en}
+        message_es={dataAbout.about.frontmatter.description_es}
+        message_en={dataAbout.about.frontmatter.description_en}
         title={intl.formatMessage({ id: "about.pagetitle" })}
       />
     </Layout>
