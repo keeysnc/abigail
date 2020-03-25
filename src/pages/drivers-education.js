5fcd18b7d32d66dde6bc4973bdbfb01088086eea
Nathan Keeyss <keeysnc@gmail.com>
2020-03-24 23:34:48 -0400

    add accordion to drivers education page

--- src/pages/drivers-education.js
+++ src/pages/drivers-education.js
@@ -4,9 +4,9 @@ import { FormattedMessage, injectIntl, navigate } from "gatsby-plugin-intl"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import CardRender from "../components/CardRender"
-import AccordionRender from "../components/AccordionRender"
 import CourseInfo from "../components/CourseInfo"
 import HorizontalRule from "../components/HorizontalRule"
+import Accordion from "../components/Accordion"
 
 const DriversEd = ({ intl }) => {
   return (
@@ -20,23 +20,128 @@ const DriversEd = ({ intl }) => {
         message={intl.formatMessage({ id: "drivers_education.message" })}
       />
       <HorizontalRule />
-      <CourseInfo
-        courseName={intl.formatMessage({
-          id: "drivers_education.course.title",
-        })}
-        description={intl.formatMessage({
-          id: "drivers_education.course.description",
-        })}
-        price={intl.formatMessage({ id: "drivers_education.course.price" })}
-        topics={intl.formatMessage({ id: "drivers_education.course.topics" })}
-        regulations={intl.formatMessage({
-          id: "drivers_education.course.regulations",
-        })}
-        requirements={intl.formatMessage({
-          id: "drivers_education.course.requirements",
-        })}
-      />
-      <AccordionRender pagetitle={"Registration"} />
+      <Accordion>
+        <div label="Alligator Mississippiensis">
+          <CourseInfo
+            courseName={intl.formatMessage({
+              id: "drivers_education.course.title",
+            })}
+            description={intl.formatMessage({
+              id: "drivers_education.course.description",
+            })}
+            price={intl.formatMessage({ id: "drivers_education.course.price" })}
+            topics={intl.formatMessage({
+              id: "drivers_education.course.topics",
+            })}
+            regulations={intl.formatMessage({
+              id: "drivers_education.course.regulations",
+            })}
+            requirements={intl.formatMessage({
+              id: "drivers_education.course.requirements",
+            })}
+          />
+        </div>
+        <div label="Alligator Sinensis">
+          <CourseInfo
+            courseName={intl.formatMessage({
+              id: "drivers_education.course.title",
+            })}
+            description={intl.formatMessage({
+              id: "drivers_education.course.description",
+            })}
+            price={intl.formatMessage({ id: "drivers_education.course.price" })}
+            topics={intl.formatMessage({
+              id: "drivers_education.course.topics",
+            })}
+            regulations={intl.formatMessage({
+              id: "drivers_education.course.regulations",
+            })}
+            requirements={intl.formatMessage({
+              id: "drivers_education.course.requirements",
+            })}
+          />
+        </div>
+        <div label="Alligator Sinensis">
+          <CourseInfo
+            courseName={intl.formatMessage({
+              id: "drivers_education.course.title",
+            })}
+            description={intl.formatMessage({
+              id: "drivers_education.course.description",
+            })}
+            price={intl.formatMessage({ id: "drivers_education.course.price" })}
+            topics={intl.formatMessage({
+              id: "drivers_education.course.topics",
+            })}
+            regulations={intl.formatMessage({
+              id: "drivers_education.course.regulations",
+            })}
+            requirements={intl.formatMessage({
+              id: "drivers_education.course.requirements",
+            })}
+          />
+        </div>
+        <div label="Alligator Sinensis">
+          <CourseInfo
+            courseName={intl.formatMessage({
+              id: "drivers_education.course.title",
+            })}
+            description={intl.formatMessage({
+              id: "drivers_education.course.description",
+            })}
+            price={intl.formatMessage({ id: "drivers_education.course.price" })}
+            topics={intl.formatMessage({
+              id: "drivers_education.course.topics",
+            })}
+            regulations={intl.formatMessage({
+              id: "drivers_education.course.regulations",
+            })}
+            requirements={intl.formatMessage({
+              id: "drivers_education.course.requirements",
+            })}
+          />
+        </div>
+        <div label="Alligator Sinensis">
+          <CourseInfo
+            courseName={intl.formatMessage({
+              id: "drivers_education.course.title",
+            })}
+            description={intl.formatMessage({
+              id: "drivers_education.course.description",
+            })}
+            price={intl.formatMessage({ id: "drivers_education.course.price" })}
+            topics={intl.formatMessage({
+              id: "drivers_education.course.topics",
+            })}
+            regulations={intl.formatMessage({
+              id: "drivers_education.course.regulations",
+            })}
+            requirements={intl.formatMessage({
+              id: "drivers_education.course.requirements",
+            })}
+          />
+        </div>
+        <div label="Alligator Sinensis">
+          <CourseInfo
+            courseName={intl.formatMessage({
+              id: "drivers_education.course.title",
+            })}
+            description={intl.formatMessage({
+              id: "drivers_education.course.description",
+            })}
+            price={intl.formatMessage({ id: "drivers_education.course.price" })}
+            topics={intl.formatMessage({
+              id: "drivers_education.course.topics",
+            })}
+            regulations={intl.formatMessage({
+              id: "drivers_education.course.regulations",
+            })}
+            requirements={intl.formatMessage({
+              id: "drivers_education.course.requirements",
+            })}
+          />
+        </div>
+      </Accordion>
     </Layout>
   )
 }
--- src/pages/drivers-improvement.js
+++ src/pages/drivers-improvement.js
@@ -6,6 +6,7 @@ import SEO from "../components/seo"
 import CardRender from "../components/CardRender"
 import CourseInfo from "../components/CourseInfo"
 import HorizontalRule from "../components/HorizontalRule"
+import Accordion from "../components/Accordion"
 
 const DriversImprovement = ({ intl }) => {
   return (
@@ -16,22 +17,128 @@ const DriversImprovement = ({ intl }) => {
         message={intl.formatMessage({ id: "drivers_improvement.message" })}
       />
       <HorizontalRule />
-      <CourseInfo
-        courseName={intl.formatMessage({
-          id: "drivers_improvement.course.title",
-        })}
-        description={intl.formatMessage({
-          id: "drivers_improvement.course.description",
-        })}
-        price={intl.formatMessage({ id: "drivers_improvement.course.price" })}
-        topics={intl.formatMessage({ id: "drivers_improvement.course.topics" })}
-        regulations={intl.formatMessage({
-          id: "drivers_improvement.course.regulations",
-        })}
-        requirements={intl.formatMessage({
-          id: "drivers_improvement.course.requirements",
-        })}
-      />
+      <Accordion>
+        <div label="Alligator Mississippiensis">
+          <CourseInfo
+            courseName={intl.formatMessage({
+              id: "drivers_education.course.title",
+            })}
+            description={intl.formatMessage({
+              id: "drivers_education.course.description",
+            })}
+            price={intl.formatMessage({ id: "drivers_education.course.price" })}
+            topics={intl.formatMessage({
+              id: "drivers_education.course.topics",
+            })}
+            regulations={intl.formatMessage({
+              id: "drivers_education.course.regulations",
+            })}
+            requirements={intl.formatMessage({
+              id: "drivers_education.course.requirements",
+            })}
+          />
+        </div>
+        <div label="Alligator Sinensis">
+          <CourseInfo
+            courseName={intl.formatMessage({
+              id: "drivers_education.course.title",
+            })}
+            description={intl.formatMessage({
+              id: "drivers_education.course.description",
+            })}
+            price={intl.formatMessage({ id: "drivers_education.course.price" })}
+            topics={intl.formatMessage({
+              id: "drivers_education.course.topics",
+            })}
+            regulations={intl.formatMessage({
+              id: "drivers_education.course.regulations",
+            })}
+            requirements={intl.formatMessage({
+              id: "drivers_education.course.requirements",
+            })}
+          />
+        </div>
+        <div label="Alligator Sinensis">
+          <CourseInfo
+            courseName={intl.formatMessage({
+              id: "drivers_education.course.title",
+            })}
+            description={intl.formatMessage({
+              id: "drivers_education.course.description",
+            })}
+            price={intl.formatMessage({ id: "drivers_education.course.price" })}
+            topics={intl.formatMessage({
+              id: "drivers_education.course.topics",
+            })}
+            regulations={intl.formatMessage({
+              id: "drivers_education.course.regulations",
+            })}
+            requirements={intl.formatMessage({
+              id: "drivers_education.course.requirements",
+            })}
+          />
+        </div>
+        <div label="Alligator Sinensis">
+          <CourseInfo
+            courseName={intl.formatMessage({
+              id: "drivers_education.course.title",
+            })}
+            description={intl.formatMessage({
+              id: "drivers_education.course.description",
+            })}
+            price={intl.formatMessage({ id: "drivers_education.course.price" })}
+            topics={intl.formatMessage({
+              id: "drivers_education.course.topics",
+            })}
+            regulations={intl.formatMessage({
+              id: "drivers_education.course.regulations",
+            })}
+            requirements={intl.formatMessage({
+              id: "drivers_education.course.requirements",
+            })}
+          />
+        </div>
+        <div label="Alligator Sinensis">
+          <CourseInfo
+            courseName={intl.formatMessage({
+              id: "drivers_education.course.title",
+            })}
+            description={intl.formatMessage({
+              id: "drivers_education.course.description",
+            })}
+            price={intl.formatMessage({ id: "drivers_education.course.price" })}
+            topics={intl.formatMessage({
+              id: "drivers_education.course.topics",
+            })}
+            regulations={intl.formatMessage({
+              id: "drivers_education.course.regulations",
+            })}
+            requirements={intl.formatMessage({
+              id: "drivers_education.course.requirements",
+            })}
+          />
+        </div>
+        <div label="Alligator Sinensis">
+          <CourseInfo
+            courseName={intl.formatMessage({
+              id: "drivers_education.course.title",
+            })}
+            description={intl.formatMessage({
+              id: "drivers_education.course.description",
+            })}
+            price={intl.formatMessage({ id: "drivers_education.course.price" })}
+            topics={intl.formatMessage({
+              id: "drivers_education.course.topics",
+            })}
+            regulations={intl.formatMessage({
+              id: "drivers_education.course.regulations",
+            })}
+            requirements={intl.formatMessage({
+              id: "drivers_education.course.requirements",
+            })}
+          />
+        </div>
+      </Accordion>
     </Layout>
   )
 }
